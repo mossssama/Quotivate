f2bc3a1837c24948f04be9e17518384f1d8ae3df
mossssama <mo99saleh@gmail.com>
2023-09-20 23:00:38 +0300

    Adding Hilt dependencies

--- app/src/main/java/com/newOs/quotivate/QuotesApplication.kt
+++ app/src/main/java/com/newOs/quotivate/QuotesApplication.kt
@@ -2,7 +2,9 @@ package com.newOs.quotivate
 
 import android.app.Application
 import android.content.Context
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class QuotesApplication: Application() {
 
     init {
